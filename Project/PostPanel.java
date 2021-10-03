0ded177da32450a354b244221d500bb3f015ce40
met <facebook4004@hotmail.com>
2021-10-02 22:53:29 -0400

    dropdown

--- Project/PostPanel.java
+++ Project/PostPanel.java
@@ -1,17 +1,39 @@
-import javax.swing.JPanel;
+import javax.swing.*;
 
 public class PostPanel extends JPanel {
 
 	private static final long serialVersionUID = -2494723166396755145L;
+    Grid grid;
 
 	public PostPanel() {
-		this.initUI();
+	    this.initUI();
 	}
-	
+
 	private void initUI() {
-		
-		
-		
+        JLabel label 	= new JLabel("Message: ");
+        JTextField text = new JTextField(10);
+
+        JButton send = new JButton("Send");
+
+        add(label);
+        add(text);
+        add(send);
+
+
+
+        String[] petStrings = Client.grid == null ? new String[]{"No Connection"} : new String[]{"connection"};
+
+        //if(Client.grid != null)
+            System.out.println("BANANA");
+
+        JComboBox petList = new JComboBox(petStrings);
+        petList.setSelectedIndex(0);
+
+        add(petList);
+
+        petList.addActionListener((e -> {
+            System.out.println(petList.getSelectedIndex());
+        }));
 	}
-	
+
 }
