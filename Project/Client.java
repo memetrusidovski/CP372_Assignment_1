dd4bddc85777d15b7170e2ca22728d41b3bfafbc
met <facebook4004@hotmail.com>
2021-10-04 14:53:48 -0400

    merge

--- Project/Client.java
+++ Project/Client.java
@@ -8,8 +8,8 @@ public class Client {
 	//For updating the message board
     Grid grid = null;
     MessagePanel messagePanel = new MessagePanel(grid);
-    String host;
-    int port;
+    static String host;
+    static int port;
 
     PinPanel pinPanel;
     PostPanel postPanel;
