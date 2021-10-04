c83d1041c09962f06ffbea4e910bfe0d6e25c390
elw-arduino <minecraftisawsome007+github@gmail.com>
2021-10-04 10:27:50 -0400

    Updating x,y spinners on connect

--- Project/PostPanel.java
+++ Project/PostPanel.java
@@ -28,8 +28,8 @@ public class PostPanel extends JPanel {
         
         xModel = new SpinnerNumberModel(0, 0, 0, 1);
 		yModel = new SpinnerNumberModel(0, 0, 0, 1);
-		widthModel = new SpinnerNumberModel(0, 0, 0, 1);
-		heightModel = new SpinnerNumberModel(0, 0, 0, 1);
+		widthModel = new SpinnerNumberModel(1,1,null,1);
+		heightModel = new SpinnerNumberModel(1,1,null,1);
 
 		JPanel positionPanel = new JPanel();
 		JLabel xLabel 	     = new JLabel("X:");
@@ -87,8 +87,17 @@ public class PostPanel extends JPanel {
         
 	}
 
-    public void paint() {
-	    this.initUI();
-    }
-
+	public void updateDimensions(int x, int y) throws IllegalArgumentException {
+		if(x < 0 || y < 0) {
+			throw new IllegalArgumentException("Dimensions of the grid can't be negative");
+		}
+		xModel.setMaximum(x);
+		yModel.setMaximum(y);
+	}
+	
+	public void resetDimensions() {
+		xModel.setMaximum(0);
+		yModel.setMaximum(0);
+	}
+	
 }
