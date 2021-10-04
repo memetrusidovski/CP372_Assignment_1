5467db1c42d0ee396f3efe438cb397475acfeeda
elw-arduino <minecraftisawsome007+github@gmail.com>
2021-10-04 18:10:25 -0400

    Using 1 socket per request

--- Project/Connection.java
+++ Project/Connection.java
@@ -6,33 +6,24 @@ import java.util.ArrayList;
 
 public class Connection {
 
-	private Socket socket = null;
-	private ObjectInputStream inputStream;
-	private ObjectOutputStream outputStream;
+	private String host = null;
+	private int port = -1;
 
 	public Grid connect(String host, int port) throws IOException, ClassNotFoundException {
 
-		if(socket != null && !socket.isClosed()) {
-			socket.close();
-		}
-		socket = new Socket(host,port);
 		Client.getInstance().host = host;
 		Client.getInstance().port = port;
+		this.host = host;
+		this.port = port;
 
 		//Send the request for Grid
-        outputStream = new ObjectOutputStream(socket.getOutputStream());
-        outputStream.writeObject(new Request(RequestCommand.CONNECTED));
-
-		inputStream = new ObjectInputStream(socket.getInputStream());
-
-        Grid x = (Grid) inputStream.readObject();
+        Grid x = (Grid) this.send(new Request(RequestCommand.CONNECTED));
         Client.getInstance().grid = x;
         Client.getInstance().messagePanel.grid = x;
         Client.getInstance().frame.repaint();
 
 
         this.addColorsToPost(x.colors);
-
 
         Client.getInstance().pinPanel.updateDimensions(x.width, x.height);
         Client.getInstance().postPanel.updateDimensions(x.width, x.height);
@@ -47,23 +38,26 @@ public class Connection {
 	    }
     }
 
-	public void disconnect() throws IOException {
+	public void disconnect() {
 
-		if(socket != null && !socket.isClosed()) {
-			socket.close();
-		}
+		host = null;
+		port = -1;
 
 	}
 
 	public Object send(Request request) throws IllegalStateException, IOException, ClassNotFoundException {
 
         Object obj;
-		if(socket == null || socket.isClosed()) {
+		if(host == null || port <= 0) {
 			throw new IllegalStateException("Not connected");
 		}
-        outputStream.writeObject(request);
-        
+		Socket socket = new Socket(host,port);
+		ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+		
+		outputStream.writeObject(request);
         obj = inputStream.readObject();
+        socket.close();
 
 		return obj;
 
