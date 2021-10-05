43b644590de5147ecbd2b014bc4ca5144ba15498
elw-arduino <minecraftisawsome007+github@gmail.com>
2021-10-05 10:47:48 -0400

    Synchronized access to database

--- Project/Server.java
+++ Project/Server.java
@@ -4,12 +4,15 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
+	
+	private static Database database;
+	
     public static void main(String argv[]) throws Exception {
         ArrayList<String> colors = new ArrayList<String>();
         colors.add("red");
         colors.add("blue");
 
-        Database database = new Database(50,50,colors);
+        database = new Database(50,50,colors);
 
         //Create the server Socket
         ServerSocket server = new ServerSocket(5555);
@@ -36,10 +39,15 @@ public class Server {
             System.out.println("Someone Connected");
 
             //Spin off the request into a service thread
-            Service service = new Service(database, connection);
+            Service service = new Service(connection);
             Thread thread = new Thread(service);
             thread.start();
         }
 
     }
+    
+    public static synchronized Database getDatabase() {
+    	return database;
+    }
+    
 }
